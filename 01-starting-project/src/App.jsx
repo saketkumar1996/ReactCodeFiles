f756b24d6bc22313f3aab497eae9597993a2d096
saketkumar1996 <callmesaket@gmail.com>
2024-01-27 21:49:00 +0530

    added data file and passing data as props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png"
-import componentsImg from "./assets/components.png"
+import { CORE_CONCEPTS } from "./data";
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -25,7 +25,7 @@ function CoreConcept(props){
       <img src={props.img} alt="" />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
-      
+
     </li>
   )
 }
@@ -39,12 +39,18 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
-          <CoreConcept  title="Components" description="The Core UI Building Block."
-          img={componentsImg}
+          <CoreConcept  title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description}
+          img={CORE_CONCEPTS[0].image}
           />
-          <CoreConcept/>
-          <CoreConcept/>
-          <CoreConcept/>
+          <CoreConcept  title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description}
+          img={CORE_CONCEPTS[1].image}
+          />
+          <CoreConcept  title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description}
+          img={CORE_CONCEPTS[2].image}
+          />
+          <CoreConcept  title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description}
+          img={CORE_CONCEPTS[3].image}
+          />
         </ul>
         </section>
       </main>

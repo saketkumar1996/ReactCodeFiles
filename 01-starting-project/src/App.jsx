dba7e6825ea8a24169fca8a7bbca7d828c83fae9
saketkumar1996 <callmesaket@gmail.com>
2024-01-27 21:21:21 +0530

    add: adding image to jsx

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png"
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -7,7 +9,7 @@ function genRandomInt(max) {
 function Header(){
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are

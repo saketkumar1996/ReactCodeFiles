5e6126e47b4d86fa3f0e7e8ae43526ecb90a1011
saketkumar1996 <callmesaket@gmail.com>
2024-02-01 01:15:36 +0530

    updating state based on previous state

--- 02-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 02-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,12 +4,15 @@ const Player = ({name,symbol}) => {
 
     const [isEditing,setIsEditing]= useState(false)
     function handleEditClick(){
-        setIsEditing(true)
+        setIsEditing(!isEditing)
     }
 
    let playerName=<span className="player-name">{name} </span>
+   let btnCaption='Edit'
+
    if(isEditing){
-    playerName= <input type="text" required />
+    playerName= <input type="text" required value={name} />
+    btnCaption="Save"
    }
   return (
     <li>
@@ -17,7 +20,7 @@ const Player = ({name,symbol}) => {
       {playerName}
     <span className="player-symbol">{symbol}</span>
     </span>
-    <button onClick={handleEditClick}>Edit</button>
+    <button onClick={handleEditClick}>{btnCaption}</button>
   </li>
   )
 }

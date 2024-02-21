b20d503f950f8d92af999eceed643da812b3ccf0
saketkumar1996 <callmesaket@gmail.com>
2024-02-21 09:47:34 +0530

    replaced exist code with useref hook use case

--- 04 timer project with ref and portal/src/components/Player.jsx
+++ 04 timer project with ref and portal/src/components/Player.jsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
-
+  const playerName = useRef()
   const [enteredPlayerName,setEnteredPlayerName]=useState(null)
-  const [submitted,setSubmitted]=useState(false)
-  function handleChange(event){
-    setSubmitted(false)
-    setEnteredPlayerName(event.target.value)
-  }
 
   function handleClick(){
-    setSubmitted(true)
+    setEnteredPlayerName(playerName.current.value)
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName :"unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ? enteredPlayerName :"unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName}/>
+        <input ref={playerName} type="text"/>
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
